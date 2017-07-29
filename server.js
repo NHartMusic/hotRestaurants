27111a378b402a62974477a353f96b47e26de038
Nick Hart <nhartmusic@gmail.com>
2017-07-29 11:12:48 -0700

    links

--- server.js
+++ server.js
@@ -28,6 +28,40 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 //Starts server to begin listening
 
+var tables =[{
+	name: 'Jesse',
+	phoneNumber: '555-555-5555',
+	email: 'jessesternmusic@gmail.com',
+	id: '33333'
+}, {
+	name: 'Nick',
+	phoneNumber: '555-555-5555',
+	email: 'nhartmusic@gmail.com',
+	id: '44444'
+}];
+
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/add", function(req, res) {
+  res.sendFile(path.join(__dirname, "add.html"));
+});
+
+// Get all tables
+app.get("/view", function(req, res) {
+   res.sendFile(path.join(__dirname, "view.html"))
+});
+
+//view JSON objects
+app.get("/api/tables", function(req, res) {
+  res.json(tables);
+});
+
+
+
+
 app.listen(PORT, function() {
 	console.log('App listening on PORT ' + PORT);
 });
